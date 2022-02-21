bc0876e53b05d0e31d45d68389fce2f12fe1370b
MaxiSanchez600 <maxsanchezg@gmail.com>
2022-02-21 16:11:44 -0300

    add the env port to the deploy

--- main.ts
+++ main.ts
@@ -1,9 +1,11 @@
 import newAppContainer from "./infra/containers/app.container";
 
+const port = process.env.PORT || 8000;
+
 // Creating the appContainer to get the Router dependency
 const appContainer = newAppContainer();
 
 // Listen to the Router requests
-appContainer.listen(8000, () => {
+appContainer.listen(port, () => {
   console.log("Server started on port 8000");
 });
